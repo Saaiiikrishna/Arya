a4289975e8a4df4c9f05cd73c4b11dc19250dc46
Sai krishna <saaiiikrishna@gmail.com>
2026-03-31 09:08:08 +0000

    Fixed Google Login

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  images: {
+    minimumCacheTTL: 60,
+  },
 };
 
 export default nextConfig;
